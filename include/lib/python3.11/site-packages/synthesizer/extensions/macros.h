adb635ae6f2eb16e458609c1b9a8371a663f568d
aliceemaffs <aliceemaffs@live.co.uk>
2024-06-01 11:32:02 +0000

    removing old folders to just include scripts only

--- include/lib/python3.11/site-packages/synthesizer/extensions/macros.h
+++
@@ -1,8 +0,0 @@
-/******************************************************************************
- * A C module containing macros for use in C extensions.
- *****************************************************************************/
-
-/* Define a macro to handle that bzero is non-standard. */
-#ifndef bzero
-#define bzero(b, len) (memset((b), '\0', (len)), (void)0)
-#endif
